412e2d5605858e3bbd06c83c6980b71fd1eec7b2
AJ Cooper <acooper@lcs.on.ca>
2024-11-21 14:31:49 -0500

    Initial Commit

---
+++ EnhancedTodoListTests/EnhancedTodoListTests.swift
@@ -0,0 +1,17 @@
+//
+//  EnhancedTodoListTests.swift
+//  EnhancedTodoListTests
+//
+//  Created by  Adam-James  Cooper on 2024-11-21.
+//
+
+import Testing
+@testable import EnhancedTodoList
+
+struct EnhancedTodoListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ EnhancedTodoListUITests/EnhancedTodoListUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  EnhancedTodoListUITestsLaunchTests.swift
+//  EnhancedTodoListUITests
+//
+//  Created by  Adam-James  Cooper on 2024-11-21.
+//
+
+import XCTest
+
+final class EnhancedTodoListUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
